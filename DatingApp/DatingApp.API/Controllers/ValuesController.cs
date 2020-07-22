b960f8d4d70d82f3ee592289de3b2ad51b5df2c5
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-07-22 16:56:19 +0700

    Create Angular project

--- DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -18,17 +19,17 @@ namespace DatingApp.API.Controllers
             this._context = context;
         }
         [HttpGet]
-        public IActionResult GetAllValues()
+        public async Task<IActionResult> GetAllValues()
         {
-            var values = _context.Values.ToList();
+            var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(el => el.Id==id);
+            var value = await _context.Values.FirstOrDefaultAsync(el => el.Id==id);
             return Ok(value);
         }
 
