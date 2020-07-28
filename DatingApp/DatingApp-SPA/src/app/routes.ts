eb852cb00c6a364965cc06c74505c25f588c4806
NguyenTuanHung14 <56195033+NguyenTuanHung14@users.noreply.github.com>
2020-07-28 23:06:33 +0700

    protecting routes

--- DatingApp/DatingApp-SPA/src/app/routes.ts
+++ DatingApp/DatingApp-SPA/src/app/routes.ts
@@ -3,10 +3,18 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberListsComponent } from './member-lists/member-lists.component';
+import { AuthGuard } from './_guards/auth.guard';
 export const appRoutes: Routes = [
-    {path:'home',component:HomeComponent},
-    {path:'members',component:MemberListsComponent},
-    {path:'messages',component:MessagesComponent},
-    {path:'lists',component:ListsComponent},
-    {path:'**',redirectTo:'home',pathMatch:'full'}
+    {path:'',component:HomeComponent},
+    {
+        path:'',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children:[
+            {path:'members',component:MemberListsComponent},
+            {path:'messages',component:MessagesComponent},
+            {path:'lists',component:ListsComponent},
+        ]
+    },
+    {path:'**',redirectTo:'',pathMatch:'full'}
 ] 
